edf00f32bc13851448881c228d54ba2efd728ca0
Rein McKelway <95264395+veil00@users.noreply.github.com>
2023-06-26 22:19:40 -0400

    Update frtloop.java
    fixing start pos

--- frtloop.java
+++ frtloop.java
@@ -50,7 +50,7 @@ public class frtloop extends JFrame {
 
         g2d.setColor(Color.BLACK);
         int startX = width / 2;
-        int startY = height - 50;
+        int startY = height - 10;
         int angle = -90;
         int depth = 10;
 
