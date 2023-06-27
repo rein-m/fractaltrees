f9f9dc01f028c3dca2976170064dfbb8b7d63581
Rein McKelway <95264395+veil00@users.noreply.github.com>
2023-06-26 22:19:27 -0400

    Update frtrandom.java
    fixing start pos

--- frtrandom.java
+++ frtrandom.java
@@ -49,7 +49,7 @@ public class frtrandom extends JFrame {
 
         g2d.setColor(Color.BLACK);
         int startX = width / 2;
-        int startY = height - 50;
+        int startY = height - 10;
         double angle = -90;
         int depth = 10;
 
